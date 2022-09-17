16621a86a49e80e30bd35c44c3cf8006c06512a8
WangZhisong <2659460681@qq.com>
2022-09-17 18:31:43 +0800

    Gin框架AHP程序

--- src/AHP_Gin/main.go
+++ src/AHP_Gin/main.go
@@ -1,8 +1,27 @@
 package main
 
-import "demoProject/src/AHP_Gin/routers"
+import (
+	"demoProject/src/AHP_Gin/global"
+	"demoProject/src/AHP_Gin/routers"
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"os"
+)
+
+func init() {
+
+	err := global.SetupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+
+}
 
 func main() {
+	f, _ := os.Create("./logs/gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+	gin.SetMode(global.ServerSetting.RunMode)
 	r := routers.Routers()
-	r.Run(":9090")
+	r.Run(":" + global.ServerSetting.HttpPort)
 }
